9ebc96ab4c2d12df1cad94aa1addc07bb8c85fb9
Yash Jangid <112190049+yashcx84@users.noreply.github.com>
2024-07-18 18:20:04 +0530

    Update Login.jsx, Deployed URL

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -24,7 +24,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:4000/api/auth/login", {
+      const response = await fetch("https://codefuez-server.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
